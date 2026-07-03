a998cfd73d6d31bc4c34d7d934589ffabdd3c2f8
alamiya iram <huzifa09487@gmail.com>
2026-07-04 00:17:19 +0530

    Add Q1

--- task13_Q1.py
+++ task13_Q1.py
@@ -1,11 +1,15 @@
 import numpy as np
-arr1 = np.array([10,20,30,40,50])
+arr1 = np.array([10, 20, 30, 40, 50])
 arr2 = np.array([[1, 2, 3],
                  [4, 5, 6],
                  [7, 8, 9]])
 print("1D Array:")
 print(arr1)
 print("Shape:", arr1.shape)
+print("Data Type:", arr1.dtype)
+
+print()
+
 print("2D Array:")
 print(arr2)
 print("Shape:", arr2.shape)
